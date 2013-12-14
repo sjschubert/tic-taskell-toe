d2735a4712afe8f50960ab1c4dc75dadfc737430
Stephen Diehl <stephen.m.diehl@gmail.com>
2013-12-14 17:05:56 -0600

    use some haskell idioms

--- src/Main.hs
+++ src/Main.hs
@@ -17,57 +17,57 @@ main = do
   putStrLn "\n Tic Taskell Toe \n"
   putStrLn help
 
-  gs <- initGame  
+  gs <- initGame
   loop gs
 
   where
-    nextState p gs 
+    nextState p gs
       | isAI p = return $ playFor p gs
       | otherwise = interactive p gs
 
     loop gs = do
         putStrLn $ renderPrettyBoard $ board gs
-        ns <- nextState (currentPlayer gs) gs        
-        
+        ns <- nextState (currentPlayer gs) gs
+
         let bs = checkBoard $ board ns
         case bs of
           Playable -> loop ns
-          _        -> (putStrLn $ renderWinner bs $ board ns) >> exitSuccess        
-            
+          _        -> (putStrLn $ renderWinner bs $ board ns) >> exitSuccess
+
 interactive :: Player -> GameState -> IO GameState
-interactive p gs = do 
+interactive p gs = do
   putStr $ promptPlayer p
   hFlush stdout
 
   l <- getLine
   let c = maybeRead l
 
-  case c of  
-    Just i 
+  case c of
+    Just i
       | (\d -> d `elem` [1..9]) i -> do
-          either 
+          either
             (\s -> putStrLn ("\n" ++ s) >> return gs)
             (\ns -> return ns)
-            (playTurn p gs (i-1))  
+            (playTurn p gs (i-1))
       | otherwise -> (putStrLn "Invalid choice!\n") >> return gs
-    Nothing -> if l == "q" 
-                then (putStrLn help) >> return gs 
+    Nothing -> if l == "q"
+                then (putStrLn help) >> return gs
                 else (putStrLn "Invalid choice!\n") >> return gs
-    
+
 initGame :: IO GameState
-initGame = do 
+initGame = do
   putStrLn "What game configuration would you like?"
   putStrLn "  1) Player(X) vs. Player(O)"
   putStrLn "  2) Player(X) vs. AI(O)"
   putStrLn "  3) AI(X) vs. Player(O)"
-  
+
   putStr "\n  Choose configuration: "
   hFlush stdout
 
-  c <- getLine 
+  c <- getLine
   let a = maybeRead c
 
-  case a of 
+  case a of
     Just 1  -> return (GameState initBoard (Player 1 False) (Player 2 False))
     Just 2  -> return (GameState initBoard (Player 1 False) (Player 2 True))
     Just 3  -> return (GameState initBoard (Player 1 True) (Player 2 False))
--- src/TTT/Game.hs
+++ src/TTT/Game.hs
@@ -1,6 +1,6 @@
 module TTT.Game (
   Tile(..),
-  Board(..),
+  Board,
   BoardState(..),
   Player(..),
   GameState(..),
@@ -8,15 +8,11 @@ module TTT.Game (
   currentPlayer,
   initBoard,
   playTurn
-) 
-
-where
-
-import Data.List
+) where
 
 data Tile = X | O | Empty deriving (Eq)
 data BoardState = XWon | OWon | Draw | Playable deriving (Eq)
-type Board = [Tile] 
+type Board = [Tile]
 
 data Player = Player {
   playerId :: Int,
@@ -26,11 +22,11 @@ data Player = Player {
 data GameState = GameState {
   board :: Board,
   playerOne :: Player,
-  playerTwo :: Player  
+  playerTwo :: Player
 }
 
 checkDim :: Tile -> Board -> [Int] -> Bool
-checkDim t b xs = all id (map (\i -> (b !! i) == t) xs) 
+checkDim t b xs = and (map (\i -> (b !! i) == t) xs)
 
 checkBoard :: Board -> BoardState
 checkBoard b
@@ -40,22 +36,22 @@ checkBoard b
   | otherwise                                       = Draw
 
 currentPlayer :: GameState -> Player
-currentPlayer gs = 
+currentPlayer gs =
   if (x == o)
     then playerOne gs
-    else playerTwo gs    
+    else playerTwo gs
   where
     x = foldl (\acc t -> if t == X then (acc + 1) else acc) 0 $ board gs
     o = foldl (\acc t -> if t == O then (acc + 1) else acc) 0 $ board gs
 
 initBoard :: Board
-initBoard = foldl (\acc t -> Empty : acc) [] [1..9]
- 
+initBoard = replicate 9 Empty
+
 playTurn :: Player -> GameState -> Int -> Either String GameState
 playTurn p gs i
   | canPlay gs i = Right $ gs {board = updateBoard i (playFor gs p) (board gs)}
   | otherwise    = Left $ "Tile " ++ (show i) ++ " is not empty."
-  where   
+  where
     playFor gs p
       | p == playerOne gs = X
       | otherwise = O
@@ -68,6 +64,6 @@ updateBoard i e (t:ts)
    | otherwise = t : updateBoard (i-1) e ts
 
 --indexes of win conditions
-winStates = [[0,1,2], [3,4,5], [6,7,8], --rows 
+winStates = [[0,1,2], [3,4,5], [6,7,8], --rows
              [0,3,6], [1,4,7], [2,5,8], --columns
              [0,4,8], [2,4,6]]          --diagonals
